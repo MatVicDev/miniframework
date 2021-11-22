654c96621f661f09c1cbae744881e1b1bd1d0ba9
MatVicDev <matheus@teste.com>
2021-11-22 10:59:29 -0300

    Adicionando o composer e configurando-o

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '02372c5482dc52ab7cc7e6cfcf926f818278977f',
+        'name' => 'miniframework/vendor',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'miniframework/vendor' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '02372c5482dc52ab7cc7e6cfcf926f818278977f',
+            'dev_requirement' => false,
+        ),
+    ),
+);
