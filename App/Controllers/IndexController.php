2829ec6ca9ae71e3660806e28cacfceb9abf21f2
MatVicDev <matheus@teste.com>
2021-11-23 09:19:20 -0300

    Trabalho com as views dinâmicamente

--- App/Controllers/IndexController.php
+++ App/Controllers/IndexController.php
@@ -4,9 +4,26 @@ namespace App\Controllers;
 
 class indexController
 {
+	private $view;
+
+	public function __construct()
+	{
+		$this->view = new \stdClass(); // Criando um objeto padrão
+	}
+
 	public function index()
 	{
-		echo 'Hello, world!';
+		$this->view->dados = ['Matheus', 'Ellen', 'Maria'];
+		$this->render('index');
+	}
+
+	public function render($view) // Método para retornar uma view
+	{
+		$class = get_class($this); // Pega a classe atual
+		$class = str_replace("App\\Controllers\\", "", $class); // Pega o nome da classe
+		$class = strtolower(str_replace("Controller", "", $class)); // Pega apenas o primeiro nome
+
+		require_once "../App/Views/".$class."/".$view.".phtml"; // Um require dinâmico
 	}
 }
 ?>
