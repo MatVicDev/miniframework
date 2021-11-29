cbf8692480aabda95c38404f383673a41674a4e3
Matheus Victor <77130879+MatVicDev@users.noreply.github.com>
2021-11-29 09:10:28 -0300

    Removendo códigos obsoletos.

--- App/Controllers/IndexController.php
+++ App/Controllers/IndexController.php
@@ -5,9 +5,6 @@ namespace App\Controllers;
 use MF\Controller\Action; // Importando a classe Action
 use MF\Model\Container;
 
-use App\Models\Produto;
-use App\Models\Info;
-
 class indexController extends Action // Extendendo de uma classe abstrata
 {
 	public function index()
